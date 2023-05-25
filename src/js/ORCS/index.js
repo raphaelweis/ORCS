d5264198088c731c47316454c7a7db501f1502f1
Raphaël Weis <raphael.weis.2003@gmail.com>
2023-05-25 10:23:43 +0200

    removed index.js, was causing problemes with vite

--- src/js/ORCS/index.js
+++
@@ -1,5 +0,0 @@
-export { Cublet, CenterPiece, EdgePiece, CornerPiece } from './lib/Cublet';
-export { default as DoublyCircularLinkedList } from './lib/DoublyCircularLinkedList';
-export { default as Face } from './lib/Face';
-export { default as MainCublet } from './lib/MainCublet';
-export { default as RubiksCube } from './lib/RubiksCube';
