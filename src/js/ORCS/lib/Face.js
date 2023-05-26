681c5bec807147798d7cef9d3a73f9feafd2de71
Raphaël Weis <raphael.weis.2003@gmail.com>
2023-05-26 14:04:15 +0200

    first fully functionnal cube.

--- src/js/ORCS/lib/Face.js
+++ src/js/ORCS/lib/Face.js
@@ -33,7 +33,7 @@ export default class Face {
         switch (this.faceID) {
             case "U":
                 this.direction = new THREE.Vector3(0, 1, 0);
-                this.coordinates = {x1: -1, x2: 1, y1: -1, y2: 1, z1: 1, z2: 1};
+                this.coordinates = {x1: -1, x2: 1, y1: 1, y2: 1, z1: -1, z2: 1};
 
                 this.mesh.lookAt(this.direction);
                 this.mesh.position.addScaledVector(this.direction, 1);
@@ -41,7 +41,7 @@ export default class Face {
                 break;
             case "D":
                 this.direction = new THREE.Vector3(0, -1, 0);
-                this.coordinates = {x1: -1, x2: 1, y1: -1, y2: 1, z1: -1, z2: -1};
+                this.coordinates = {x1: -1, x2: 1, y1: -1, y2: -1, z1: -1, z2: 1};
 
                 this.mesh.lookAt(this.direction);
                 this.mesh.position.addScaledVector(this.direction, 1);
@@ -73,7 +73,7 @@ export default class Face {
                 break;
             case "B":
                 this.direction = new THREE.Vector3(0, 0, -1);
-                this.direction = {x1: -1, x2: 1, y1: -1, y2: 1, z1: -1, z2: -1};
+                this.coordinates = {x1: -1, x2: 1, y1: -1, y2: 1, z1: -1, z2: -1};
 
                 this.mesh.lookAt(this.direction);
                 this.mesh.position.addScaledVector(this.direction, 1);
@@ -82,31 +82,50 @@ export default class Face {
         }
     }
 
-    // updateEdgePieces() {
-    //     let edgesPiecesToRotate = [];
-    //
-    //     this.rubiksCube.edgePieces.forEach((edgePiece) => {
-    //         let isInFaceX = this.coordinates.x1 <= edgePiece.mesh.position.x <= this.coordinates.x2;
-    //         let isInFaceY = this.coordinates.y1 <= edgePiece.mesh.position.y <= this.coordinates.y2;
-    //         let isInFaceZ = this.coordinates.z1 <= edgePiece.mesh.position.z <= this.coordinates.z2;
-    //
-    //         if (isInFaceX && isInFaceY && isInFaceZ) {
-    //             edgesPiecesToRotate.push(edgePiece);
-    //         }
-    //     })
-    //     return edgesPiecesToRotate;
-    // }
+    updateEdgePieces() {
+        let edgesPiecesToRotate = [];
 
-    #rotate(start, prev, end, edgePieces) {
+        this.rubiksCube.edgePieces.forEach((edgePiece) => {
+            let isInFaceX = this.coordinates.x1 <= edgePiece.mesh.position.x && edgePiece.mesh.position.x <= this.coordinates.x2;
+            let isInFaceY = this.coordinates.y1 <= edgePiece.mesh.position.y && edgePiece.mesh.position.y <= this.coordinates.y2;
+            let isInFaceZ = this.coordinates.z1 <= edgePiece.mesh.position.z && edgePiece.mesh.position.z <= this.coordinates.z2;
+
+            if (isInFaceX && isInFaceY && isInFaceZ) {
+                edgesPiecesToRotate.push(edgePiece);
+            }
+        })
+        return edgesPiecesToRotate;
+    }
+
+    updateCornerPieces() {
+        let cornerPiecesToRotate = [];
+
+        this.rubiksCube.cornerPieces.forEach((cornerPiece) => {
+            let isInFaceX = this.coordinates.x1 <= cornerPiece.mesh.position.x && cornerPiece.mesh.position.x <= this.coordinates.x2;
+            let isInFaceY = this.coordinates.y1 <= cornerPiece.mesh.position.y && cornerPiece.mesh.position.y <= this.coordinates.y2;
+            let isInFaceZ = this.coordinates.z1 <= cornerPiece.mesh.position.z && cornerPiece.mesh.position.z <= this.coordinates.z2;
+
+            if (isInFaceX && isInFaceY && isInFaceZ) {
+                cornerPiecesToRotate.push(cornerPiece);
+            }
+        })
+        return cornerPiecesToRotate;
+    }
+
+    #rotate(start, prev, end, edgePieces, cornerPieces) {
         this.rubiksCube.isAnimating = true;
         new TWEEN.Tween(start)
             .to(end, 100)
             .onUpdate(({rotation}) => {
                 this.mesh.rotateOnWorldAxis(this.direction, rotation - prev.rotation);
                 this.centerPiece.mesh.rotateOnWorldAxis(this.direction, rotation - prev.rotation);
-                edgePieces.forEach((edgePiece)=> {
+                edgePieces.forEach((edgePiece) => {
                     edgePiece.mesh.position.applyAxisAngle(this.direction, rotation - prev.rotation);
                     edgePiece.mesh.rotateOnWorldAxis(this.direction, rotation - prev.rotation);
+                })
+                cornerPieces.forEach((cornerPiece)=> {
+                    cornerPiece.mesh.position.applyAxisAngle(this.direction, rotation - prev.rotation);
+                    cornerPiece.mesh.rotateOnWorldAxis(this.direction, rotation - prev.rotation);
                 })
 
                 prev.rotation = rotation;
@@ -121,6 +140,13 @@ export default class Face {
                         Math.round(edgePiece.mesh.position.z)
                     )
                 })
+                cornerPieces.forEach((cornerPiece) => {
+                    cornerPiece.mesh.position.set(
+                        Math.round(cornerPiece.mesh.position.x),
+                        Math.round(cornerPiece.mesh.position.y),
+                        Math.round(cornerPiece.mesh.position.z)
+                    )
+                })
             })
             .start();
     }
@@ -129,17 +155,19 @@ export default class Face {
         const start = {rotation: 0};
         const prev = {rotation: 0};
         const end = {rotation: -_90Degrees};
-        // const edgePiecesToRotate = this.updateEdgePieces();
+        const edgePiecesToRotate = this.updateEdgePieces();
+        const cornerPiecesToRotate = this.updateCornerPieces();
 
-        // this.#rotate(start, prev, end, edgePiecesToRotate);
+        this.#rotate(start, prev, end, edgePiecesToRotate, cornerPiecesToRotate);
     }
 
     rotateCounterClockwise() {
         const start = {rotation: 0};
         const prev = {rotation: 0};
         const end = {rotation: _90Degrees};
-        // const edgePiecesToRotate = this.updateEdgePieces();
+        const edgePiecesToRotate = this.updateEdgePieces();
+        const cornerPiecesToRotate = this.updateCornerPieces();
 
-        // this.#rotate(start, prev, end, edgePiecesToRotate);
+        this.#rotate(start, prev, end, edgePiecesToRotate, cornerPiecesToRotate);
     }
 }
